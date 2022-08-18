ddde463a1194762aea6a32d5ea8cd2c3cbc87610
yuta-yama-1123 <yutaforios@gmail.com>
2022-08-19 01:26:41 +0900

    プログレスバー配置したり、ボタンのレイアウト修正したり。

--- TestIntervalTimer/ContentView.swift
+++ TestIntervalTimer/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var timerContorller =  TimerModel()
+    @ObservedObject var timerController =  TimerModel()
+    @State var intervalMinute = 0 // タイマー時間(分)
+    @State var intervalSecond = 0
+    @State private var progress = 0.0
+    
+    // デフォルト値の設定
+    init() {
+        _intervalMinute = State(initialValue: 2)
+        _intervalSecond = State(initialValue: intervalMinute * 60)
+    }
+    // 画面表示設定
     var body: some View {
         VStack(alignment: .center) {
-            Button(action: {
-                if(timerContorller.timer == nil){
-                    timerContorller.start(5.00)
-                }else{
-                    timerContorller.stop()
+            Text("Interval")
+                .font(
+                    .system(
+                        size: 50,
+                        weight: .heavy,
+                        design: .rounded
+                    )
+                )
+            HStack(alignment: .center) {
+                Button(action: {
+                    if (intervalMinute > 1) {
+                        intervalMinute -= 1
+                    }
+                }){
+                    Text("↓")
+                        .font(
+                            .system(
+                                size: 70,
+                                weight: .heavy,
+                                design: .rounded
+                            )
+                        )
                 }
-            }){
-                Text("\((timerContorller.timer != nil) ? "Stop Timer" : "Start Timer")")
+                    .frame(width: 60, height: 60)
+                Text(String(intervalMinute))
                     .font(
                         .system(
                             size: 100,
@@ -26,15 +53,114 @@ struct ContentView: View {
                             design: .rounded
                         )
                     )
-            }
-            Text("\(timerContorller.countStr)")
-                .font(
-                    .system(
-                        size: 100,
-                        weight: .heavy,
-                        design: .rounded
+                Text("min")
+                    .font(
+                        .system(
+                            size: 80,
+                            weight: .semibold,
+                            design: .rounded
+                        )
                     )
+                Button(action: {
+                    intervalMinute += 1
+                }){
+                    Text("↑")
+                        .font(
+                            .system(
+                                size: 70,
+                                weight: .heavy,
+                                design: .rounded
+                            )
+                        )
+                }
+            }
+            .padding(
+                EdgeInsets(
+                    top: 1,        // 上の余白
+                    leading: 1,    // 左の余白
+                    bottom: -30,     // 下の余白
+                    trailing: 1    // 右の余白
                 )
+            )
+            .offset(y: -30)
+            Button(action: {
+                if(timerController.timer == nil){
+                    intervalSecond = intervalMinute * 60
+                    timerController.start(Double(intervalSecond))
+                }else{
+                    timerController.stop()
+                }
+            }){
+                Text("\((timerController.timer != nil) ? "Stop" : "(Re)Start")")
+                    .font(
+                        .system(
+                            size: 70,
+                            weight: .heavy,
+                            design: .rounded
+                        )
+                    )
+                    .foregroundColor(Color.white)
+                    .frame(
+                        minWidth: 300,
+                        idealWidth: 350,
+                        maxWidth: 350,
+                        minHeight: 100,
+                        idealHeight: 150,
+                        maxHeight: 220,
+                        alignment: .center)
+
+            }
+                .background(.blue)
+                .cornerRadius(100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(.indigo, lineWidth: 5.0)
+                )
+            VStack {
+                Text("Remaining:")
+                    .font(
+                        .system(
+                            size: 30,
+                            weight: .heavy,
+                            design: .rounded
+                        )
+                    )
+                Text("\(timerController.countStr)")
+                    .font(
+                        .system(
+                            size: 80,
+                            weight: .heavy,
+                            design: .rounded
+                        )
+                    )
+                }
+            .padding()
+            VStack(alignment: .leading) {
+                Text("Get Ready...")
+                    .font(
+                        .system(
+                            size:25,
+                            weight: .heavy,
+                            design: .rounded
+                        )
+                    )
+                    .padding(
+                        EdgeInsets(
+                            top: 1,        // 上の余白
+                            leading: 1,    // 左の余白
+                            bottom: 25,     // 下の余白
+                            trailing: 1    // 右の余白
+                        )
+                    )
+                ProgressView(
+                    value: timerController.remaining,
+                    total: Double(intervalSecond)
+                )
+                .accentColor(Color.green)
+                .scaleEffect(x: 1, y: 15, anchor: .center)
+            }
+            .padding()
+            .offset(y: -30)
         }
     }
 }
--- TestIntervalTimer/TimerModel.swift
+++ TestIntervalTimer/TimerModel.swift
@@ -6,35 +6,46 @@ import UIKit
 class TimerModel: ObservableObject{
     @Published var count: Double = 0.00
     @Published var timer: AnyCancellable!
-    @Published var countStr: String = ""
-    let interval: Double = 0.05
+    @Published var countStr: String = "0.00"
+    @Published var timeLimit: Double = 0.00
+    @Published var remaining: Double = 0.00
+    let interval: Double = 0.05 // タイマー処理ループ間隔
     var soundFlg: Bool = false
+    @Published var progress: Double = 0.0
     
+    // アラーム音声ファイル読み込み
     private let shining_star = try!  AVAudioPlayer(data: NSDataAsset(name: "shining_star")!.data)
 
+    // 音声ファイル再生
     private func shiningStar() {
-        shining_star.stop()//追加①
-        shining_star.currentTime = 0.0//追加②
-        shining_star.play()
+        shining_star.stop()             // 停止
+        shining_star.currentTime = 0.0  // 頭出し※最初から再生するため
+        shining_star.play()             // 再生
     }
-
+    // 音声ファイル再生停止
     private func stopShiningStar() {
-        shining_star.stop()//追加①
-        shining_star.currentTime = 0.0//追加②
+        shining_star.stop()             // 停止
+        shining_star.currentTime = 0.0  // 頭出し※最初から再生するため
     }
-    
-    // タイマーの開始
+    private func calcProgress() {
+        progress = ( (timeLimit - count) / timeLimit )
+        print(timeLimit)
+        print(count)
+        print(progress)
+    }
+    // タイマー処理の開始
     func start(_ timeRemaining: Double = 10.00){
-        count = timeRemaining
+        count = timeRemaining   // 残り時間（秒）の取得
+        timeLimit = timeRemaining
         // TimerPublisherが存在しているときは念の為処理をキャンセル
         if let _timer = timer{
             _timer.cancel()
         }
-
+        // タイマー定義
         timer = Timer
             .publish(
-                every: interval,
-                on: .main,
+                every: interval, // タイマー処理実行間隔
+                on: .main,       // 実行場所(.main:メインスレッドでの実行)
                 in: .common
             )
             .autoconnect() // 繰り返し処理の実行
@@ -47,7 +58,8 @@ class TimerModel: ObservableObject{
                         // 設定した時間ごとに呼ばれる
                         self.count -= self.interval
                         self.countStr = String(format: "%.1f", self.count)
-                        if (self.count < 0) {
+                        self.remaining += self.interval
+                        if (self.count < 0.05) {
                             self.shiningStar()
                             self.timer?.cancel()
                         }
